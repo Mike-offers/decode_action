7a80d14c394344a4968ddd7d9b1af4ee6db3823a
action <action@github.com>
2025-01-16 08:17:14 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,9 +1,11 @@
-//Thu Jan 16 2025 06:51:55 GMT+0000 (Coordinated Universal Time)
+//Thu Jan 16 2025 08:17:14 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-let body = JSON.parse($response.body);
-body.result && (body.result.schoolTrialExpirationTime = "2099-09-09", body.result.hasSchoolSubscription = true, body.result.schoolSubscriptionExpirationTime = "2099-09-09", body.result.username = "baby@baby.com");
-body = JSON.stringify(body).replace(/"subscription":\s*(null|\{[\s\S]*?\})/g, "\"subscription\": {\n    \"id\": 88888888,\n    \"subscriptionPackage\": {\n        \"imageUrls\": {\n            \"optimal\": \"https://api.v2.bookrclass.com/api/media/c3Vic2NyaXB0aW9uX3BhY2thZ2Uvcy9BL3NBMzZEYU14clkyTWxRYWVzWnJUcVBPVXUxbmh1SFQxN3hNTVd4ZmRmTi5wbmc=/original_2k.png\",\n            \"original_xl\": \"https://api.v2.bookrclass.com/api/media/c3Vic2NyaXB0aW9uX3BhY2thZ2Uvcy9BL3NBMzZEYU14clkyTWxRYWVzWnJUcVBPVXUxbmh1SFQxN3hNTVd4ZmRmTi5wbmc=/binary_256.png\",\n            \"original_s\": \"https://api.v2.bookrclass.com/api/media/c3Vic2NyaXB0aW9uX3BhY2thZ2Uvcy9BL3NBMzZEYU14clkyTWxRYWVzWnJUcVBPVXUxbmh1SFQxN3hNTVd4ZmRmTi5wbmc=/binary_128.png\",\n            \"original\": \"https://api.v2.bookrclass.com/api/media/c3Vic2NyaXB0aW9uX3BhY2thZ2Uvcy9BL3NBMzZEYU14clkyTWxRYWVzWnJUcVBPVXUxbmh1SFQxN3hNTVd4ZmRmTi5wbmc=/original.png\"\n        },\n        \"userCount\": 5,\n        \"id\": 2,\n        \"title\": \"Yearly\",\n        \"webshopId\": \"com.bookrclass.yearly\",\n        \"duration\": \"Year\",\n        \"appStoreId\": \"com.bookrclass.yearly\",\n        \"googlePlayId\": \"com.bookrclass.yearly\",\n        \"description\": \"Save money with a yearly subscription.\"\n    },\n    \"IsSchoolTrial\": false,\n    \"expirationTime\": \"2099-09-29T01:50:19.000000Z\"\n}");
+var _0x4de48c = 7;
+var _0x41191a = $response.body;
+_0x4de48c = 11;
+var _0x130c64 = $request.url;
+var _0x41191a = $response.body.replace(/[\s\S]+/g, "{   \"Header\" : {     \"Msg\" : \"查询成功\",     \"Result\" : 0   },   \"Content\" : {     \"vip_type\" : 2,     \"end_time\" : \"2999-09-09 13:40:58\",     \"system_time\" : \"2024-05-12 13:41:28\",     \"is_vip\" : 1   } }");
 $done({
-  "body": body
+  body: _0x41191a
 });
