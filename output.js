481fe6af4c99f104456163e886d02f2087eca7a4
action <action@github.com>
2025-01-25 17:25:26 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,30 +1,75 @@
-//Sat Jan 25 2025 16:51:05 GMT+0000 (Coordinated Universal Time)
+//Sat Jan 25 2025 17:25:26 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-var a0x;
-a0x = JSON.parse($response.body);
-a0x.data.ssvip.isPaidVip = true;
-a0x.data.ssvip.isVip = true;
-a0x.data.ssvip.vipExpiringDays = 9999;
-a0x.data.ssvip.isTempVip = true;
-a0x.data.svip.isPaidVip = true;
-a0x.data.svip.isVip = true;
-a0x.data.svip.vipExpiringDays = 9999;
-a0x.data.svip.isTempVip = true;
-a0x.data.vip.isPaidVip = true;
-a0x.data.vip.isVip = true;
-a0x.data.vip.vipExpiringDays = 9999;
-a0x.data.vip.isTempVip = true;
-a0x.data.trainingCampVip.isPaidVip = true;
-a0x.data.trainingCampVip.isVip = true;
-a0x.data.trainingCampVip.vipExpiringDays = 9999;
-a0x.data.trainingCampVip.isTempVip = true;
-a0x.data.vipTime = "2099-09-09 02:20:13";
-a0x.data.vipType = "1";
-a0x.data.pureSviptime = "2099-09-09 02:20:13";
-a0x.data.svipTime = "2099-09-09 02:20:13";
-a0x.data.svipType = "1";
-a0x.data.ssvipTime = "2099-09-09 02:20:13";
+var encode_version = "jsjiami.com.v5";
+var _0x43111e = $response.body;
+let _0x81f0f5 = JSON.parse($response.body);
+_0x81f0f5.id = "https://t.me/GieGie777";
+_0x81f0f5.balance = 9999;
+_0x81f0f5.vipExpiresAt = "2999-09-09T00:00:00Z";
 $done({
-  body: JSON.stringify(a0x)
+  body: JSON.stringify(_0x81f0f5)
 });
+(function (_0x2e8369, _0xdce963, _0xfc4f) {
+  var _0x595efc = function () {
+    var _0x16d50b = true;
+    return function (_0x580dd2, _0x3c2b06) {
+      {
+        var _0x57425c = _0x16d50b ? function () {
+          if (_0x3c2b06) {
+            var _0x5d4935 = _0x3c2b06.apply(_0x580dd2, arguments);
+            _0x3c2b06 = null;
+            return _0x5d4935;
+          }
+        } : function () {};
+        _0x16d50b = false;
+        return _0x57425c;
+      }
+    };
+  }();
+  var _0x106e04 = _0x595efc(this, function () {
+    {
+      var _0x24dc3f = function () {};
+      var _0x525928 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+      if (!_0x525928.console) {
+        {
+          _0x525928.console = function (_0x14b098) {
+            var _0xfc4f = {};
+            _0xfc4f.log = _0x14b098;
+            _0xfc4f.warn = _0x14b098;
+            _0xfc4f.debug = _0x14b098;
+            _0xfc4f.info = _0x14b098;
+            _0xfc4f.error = _0x14b098;
+            _0xfc4f.exception = _0x14b098;
+            _0xfc4f.trace = _0x14b098;
+            return _0xfc4f;
+          }(_0x24dc3f);
+        }
+      } else {
+        {
+          _0x525928.console.log = _0x24dc3f;
+          _0x525928.console.warn = _0x24dc3f;
+          _0x525928.console.debug = _0x24dc3f;
+          _0x525928.console.info = _0x24dc3f;
+          _0x525928.console.error = _0x24dc3f;
+          _0x525928.console.exception = _0x24dc3f;
+          _0x525928.console.trace = _0x24dc3f;
+        }
+      }
+    }
+  });
+  _0x106e04();
+  _0xfc4f = "al";
+  try {
+    _0xfc4f += "ert";
+    _0xdce963 = encode_version;
+    if (!(typeof _0xdce963 !== "undefined" && _0xdce963 === "jsjiami.com.v5")) {
+      {
+        _0x2e8369[_0xfc4f]("ɾ��版本号，js会定期弹窗，还请支持我们的工作");
+      }
+    }
+  } catch (_0x52cdf4) {
+    _0x2e8369[_0xfc4f]("删除版本号，js会定期弹窗");
+  }
+})(window);
+encode_version = "jsjiami.com.v5";
