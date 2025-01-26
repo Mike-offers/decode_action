b95795d83c6034692a345a78be61658656578857
action <action@github.com>
2025-01-26 16:28:42 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,75 +1,44 @@
-//Sat Jan 25 2025 17:25:26 GMT+0000 (Coordinated Universal Time)
+//Sun Jan 26 2025 16:28:42 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 var encode_version = "jsjiami.com.v5";
-var _0x43111e = $response.body;
-let _0x81f0f5 = JSON.parse($response.body);
-_0x81f0f5.id = "https://t.me/GieGie777";
-_0x81f0f5.balance = 9999;
-_0x81f0f5.vipExpiresAt = "2999-09-09T00:00:00Z";
-$done({
-  body: JSON.stringify(_0x81f0f5)
-});
-(function (_0x2e8369, _0xdce963, _0xfc4f) {
-  var _0x595efc = function () {
-    var _0x16d50b = true;
-    return function (_0x580dd2, _0x3c2b06) {
-      {
-        var _0x57425c = _0x16d50b ? function () {
-          if (_0x3c2b06) {
-            var _0x5d4935 = _0x3c2b06.apply(_0x580dd2, arguments);
-            _0x3c2b06 = null;
-            return _0x5d4935;
-          }
-        } : function () {};
-        _0x16d50b = false;
-        return _0x57425c;
-      }
-    };
-  }();
-  var _0x106e04 = _0x595efc(this, function () {
-    {
-      var _0x24dc3f = function () {};
-      var _0x525928 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
-      if (!_0x525928.console) {
-        {
-          _0x525928.console = function (_0x14b098) {
-            var _0xfc4f = {};
-            _0xfc4f.log = _0x14b098;
-            _0xfc4f.warn = _0x14b098;
-            _0xfc4f.debug = _0x14b098;
-            _0xfc4f.info = _0x14b098;
-            _0xfc4f.error = _0x14b098;
-            _0xfc4f.exception = _0x14b098;
-            _0xfc4f.trace = _0x14b098;
-            return _0xfc4f;
-          }(_0x24dc3f);
-        }
-      } else {
-        {
-          _0x525928.console.log = _0x24dc3f;
-          _0x525928.console.warn = _0x24dc3f;
-          _0x525928.console.debug = _0x24dc3f;
-          _0x525928.console.info = _0x24dc3f;
-          _0x525928.console.error = _0x24dc3f;
-          _0x525928.console.exception = _0x24dc3f;
-          _0x525928.console.trace = _0x24dc3f;
-        }
-      }
-    }
-  });
-  _0x106e04();
-  _0xfc4f = "al";
+var body = $response.body;
+var url = $request.url;
+const p1 = "/daquan2_rest.action?manhua";
+const p2 = "?type=";
+if (typeof body === "string") {
+  if (url.indexOf(p1) !== -1) {
+    body = body.replace(/"coinPrice":\d+/g, "\"coinPrice\" : 0");
+  }
+  var obj;
   try {
-    _0xfc4f += "ert";
-    _0xdce963 = encode_version;
-    if (!(typeof _0xdce963 !== "undefined" && _0xdce963 === "jsjiami.com.v5")) {
-      {
-        _0x2e8369[_0xfc4f]("ɾ��版本号，js会定期弹窗，还请支持我们的工作");
-      }
+    obj = JSON.parse(body);
+    if (url.indexOf(p2) !== -1) {
+      obj.user.chatName = "https://t.me/GieGie777";
+      obj.user.avatarUrl = "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg";
+      obj.user.vipExpiredTime = "2999-09-09";
+      obj.user.userTypeName = "永久VIP";
+      obj.user.message = "����VIP";
+      obj.user.userType = 1;
+      body = JSON.stringify(obj);
     }
-  } catch (_0x52cdf4) {
-    _0x2e8369[_0xfc4f]("删除版本号，js会定期弹窗");
+  } catch (_0xa67507) {
+    console.log("JSON 解析错误: " + _0xa67507.message);
+  }
+}
+$done({
+  body: body
+});
+(function (_0x4ff265, _0x2a41e5, _0x3b11c2) {
+  _0x3b11c2 = "al";
+  try {
+    _0x3b11c2 += "ert";
+    _0x2a41e5 = encode_version;
+    if (!(typeof _0x2a41e5 !== "undefined" && _0x2a41e5 === "jsjiami.com.v5")) {
+      _0x4ff265[_0x3b11c2]("ɾ��版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x3eb9d7) {
+    _0x4ff265[_0x3b11c2]("ɾ���汾�ţ�js�ᶨ�ڵ���");
   }
 })(window);
 encode_version = "jsjiami.com.v5";
