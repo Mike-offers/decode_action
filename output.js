289b3b86586b44add6c56780c1974df5e2f40c5c
action <action@github.com>
2025-05-07 16:24:28 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,41 +1,80 @@
-//Sun May 04 2025 13:00:39 GMT+0000 (Coordinated Universal Time)
+//Wed May 07 2025 16:24:28 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var ojbk = JSON.parse($response.body);
-const vip = "/config",
-  vip1 = "/redeem",
-  vip2 = "/com";
-$request.url.indexOf(vip) != -1 && (ojbk = {
-  "code": 200,
-  "msg": "成功",
-  "data": {
-    "open": true,
-    "open_default": false,
-    "param": "",
-    "param_default": ""
-  }
-});
-$request.url.indexOf(vip1) != -1 && (ojbk = {
-  "code": 200,
-  "msg": "兑换成功",
-  "data": {
-    "idRewards": [{
-      "id": 20001,
-      "count": 99970
-    }, {
-      "id": 20002,
-      "count": 9900000
-    }, {
-      "id": 20003,
-      "count": 91000
-    }, {
-      "id": 20004,
-      "count": 9900000
-    }],
-    "typeRewards": {}
-  }
-});
-$request.url.indexOf(vip2) != -1 && (ojbk.body = ojbk.body.replace(/\"c":"\d+"/g, "\"c\":\"0\""));
+var encode_version = "jsjiami.com.v5";
+var _0x241aaa = $request.headers;
+_0x241aaa.Cookie = "MUSIC_U=00CD11039308D14DFD39BF180DDD24D0C0E9D9A919644A4D74388219678336AA1CA0ADEEA7F19CC33E476AB952B50650766361EC504C72C984905C7F00362347DE7491CDBAC10662D4DE168723AE55FE44A721E0B62C25861FE1A96F8744FCC14A977C87B6925C3AA4057C83BC22D2899FA1BCE9D3343D96DF26D2D44D254F7F0C1DB7F2728A8960F6211A1488311109FCEDA36C1FBFED0A0557C2001546B22CBD3BC6E0E660FC21C3844C9D9C20B44CBDC319AA557DE4CFE08B9EE472E9E90866C757BFCCB3B6703CA721AE874C804C8BE47E946C1F8EA7376166AB720F995D482999CD6BF2815224BFE0E1EB3CB40E66BC26FA071C57FE5A2EBD7807C16777B558FEB8432209CE82FA808D22A023CCBDBCA2CD23DD255291B8E24C19BB723656;appver=9.1.90; deviceId=9471bf34a33036f50121ecba0591a4bc; os=iPhone OS; osver=16.2; machineid=iPhone13.4; ";
+_0x241aaa["User-Agent"] = "NeteaseMusic 9.1.90/5464 (iPhone; iOS 16.2; zh_CN)";
+_0x241aaa["MConfig-Info"] = "{\"zr4bw6pKFDIZScpo\":{\"version\":1931264,\"appver\":\"9.1.90\"},\"tPJJnts2H31BZXmp\":{\"version\":3303424,\"appver\":\"2.0.30\"},\"c0Ve6C0uNl2Am0Rl\":{\"version\":598016,\"appver\":\"1.7.50\"},\"IuRPVVmc3WWul9fT\":{\"version\":58468352,\"appver\":\"9.1.90\"}}";
 $done({
-  "body": JSON.stringify(ojbk)
+  headers: _0x241aaa
 });
+(function (_0x456097, _0x3a47d6, _0x25b0bd) {
+  var _0xb140 = function () {
+    {
+      var _0x2d8e98 = true;
+      return function (_0x29e6f2, _0x3842da) {
+        {
+          var _0x228067 = _0x2d8e98 ? function () {
+            {
+              if (_0x3842da) {
+                {
+                  var _0x4dcd7b = _0x3842da.apply(_0x29e6f2, arguments);
+                  _0x3842da = null;
+                  return _0x4dcd7b;
+                }
+              }
+            }
+          } : function () {};
+          _0x2d8e98 = false;
+          return _0x228067;
+        }
+      };
+    }
+  }();
+  var _0x2497ad = _0xb140(this, function () {
+    {
+      var _0x10c0ab = function () {};
+      var _0x21ab8d = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+      if (!_0x21ab8d.console) {
+        {
+          _0x21ab8d.console = function (_0x152329) {
+            var _0x25b0bd = {};
+            _0x25b0bd.log = _0x152329;
+            _0x25b0bd.warn = _0x152329;
+            _0x25b0bd.debug = _0x152329;
+            _0x25b0bd.info = _0x152329;
+            _0x25b0bd.error = _0x152329;
+            _0x25b0bd.exception = _0x152329;
+            _0x25b0bd.trace = _0x152329;
+            return _0x25b0bd;
+          }(_0x10c0ab);
+        }
+      } else {
+        {
+          _0x21ab8d.console.log = _0x10c0ab;
+          _0x21ab8d.console.warn = _0x10c0ab;
+          _0x21ab8d.console.debug = _0x10c0ab;
+          _0x21ab8d.console.info = _0x10c0ab;
+          _0x21ab8d.console.error = _0x10c0ab;
+          _0x21ab8d.console.exception = _0x10c0ab;
+          _0x21ab8d.console.trace = _0x10c0ab;
+        }
+      }
+    }
+  });
+  _0x2497ad();
+  _0x25b0bd = "al";
+  try {
+    _0x25b0bd += "ert";
+    _0x3a47d6 = encode_version;
+    if (!(typeof _0x3a47d6 !== "undefined" && _0x3a47d6 === "jsjiami.com.v5")) {
+      _0x456097[_0x25b0bd]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x39f4) {
+    {
+      _0x456097[_0x25b0bd]("删除版本号，js会定期弹窗");
+    }
+  }
+})(window);
+encode_version = "jsjiami.com.v5";
