b5c9961832152d66ea89cd53d2c46d633c84b8f3
action <action@github.com>
2025-05-24 03:25:48 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,115 +1,153 @@
-//Sat May 24 2025 03:05:27 GMT+0000 (Coordinated Universal Time)
+//Sat May 24 2025 03:25:48 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var t,
-  n = ["YXBwbHk=", "ZnVuY3Rpb24gKlwoICpcKQ==", "XCtcKyAqKD86XzB4KD86W2EtZjAtOV0pezQsNn18KD86XGJ8XGQpW2EtejAtOV17MSw0fSg/OlxifFxkKSk=", "dGVzdA==", "Y2hhaW4=", "aW5wdXQ=", "cmV0dXJuIChmdW5jdGlvbigpIA==", "e30uY29uc3RydWN0b3IoInJldHVybiB0aGlzIikoICk=", "Y29uc29sZQ==", "d2Fybg==", "ZGVidWc=", "ZXhjZXB0aW9u", "bG9n", "aW5mbw==", "ZXJyb3I=", "dHJhY2U=", "cGFyc2U=", "Ym9keQ==", "ZGF0YQ==", "cHJlbWl1bS5mb3JldmVy", "cGVybWlzc2lvbnM=", "cHJlbWl1bQ==", "cmVndWxhcg==", "cHVyY2hhc2U=", "cHJvZHVjdGlvbg==", "MTIwMDAyMzM1NjY2MzIx", "c3Vic2NyaXB0aW9uX3N0YXJ0ZWQ=", "YXBwbGVfZXh0cmE=", "b3JpZ2luYWxfYXBwbGljYXRpb25fdmVyc2lvbg==", "c3RyaW5naWZ5", "c3RyaW5n", "d2hpbGUgKHRydWUpIHt9", "Y291bnRlcg==", "bGVuZ3Ro", "Y29uc3RydWN0b3I=", "ZGVidQ==", "Z2dlcg==", "Y2FsbA==", "YWN0aW9u", "c3RhdGVPYmplY3Q="],
-  r = function (t, c) {
-    t -= 0;
-    var e = n[t];
-    undefined === r.xMqVtb && (!function () {
-      var t;
-      try {
-        var n = Function('return (function() {}.constructor("return this")( ));');
-        t = n();
-      } catch (n) {
-        t = window;
-      }
-      t.atob || (t.atob = function (t) {
-        for (var n, r, c = String(t).replace(/=+$/, ""), e = 0, o = 0, i = ""; r = c.charAt(o++); ~r && (n = e % 4 ? 64 * n + r : r, e++ % 4) ? i += String.fromCharCode(255 & n >> (-2 * e & 6)) : 0) r = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".indexOf(r);
-        return i;
-      });
-    }(), r.VccfBh = function (t) {
-      for (var n = atob(t), r = [], c = 0, e = n.length; c < e; c++) r += "%" + ("00" + n.charCodeAt(c).toString(16)).slice(-2);
-      return decodeURIComponent(r);
-    }, r.YBqYUl = {}, r.xMqVtb = true);
-    var o = r.YBqYUl[t];
-    return undefined === o ? (e = r.VccfBh(e), r.YBqYUl[t] = e) : e = o, e;
-  },
-  c = (t = true, function (n, c) {
-    var e = t ? function () {
-      if (c) {
-        var t = c[r("0x0")](n, arguments);
-        return c = null, t;
-      }
-    } : function () {};
-    return t = false, e;
-  });
-!function () {
-  c(this, function () {
-    var t = new RegExp(r("0x1")),
-      n = new RegExp(r("0x2"), "i"),
-      c = x("init");
-    t[r("0x3")](c + r("0x4")) && n.test(c + r("0x5")) ? x() : c("0");
-  })();
-}();
-var e,
-  o,
-  i = (e = true, function (t, n) {
-    var c = e ? function () {
-      if (n) {
-        var c = n[r("0x0")](t, arguments);
-        return n = null, c;
-      }
-    } : function () {};
-    return e = false, c;
-  }),
-  a = i(this, function () {
-    var t,
-      n = function () {};
-    try {
-      var c = Function(r("0x6") + r("0x7") + ");");
-      t = c();
-    } catch (n) {
-      t = window;
-    }
-    t[r("0x8")] ? (t[r("0x8")][r("0xc")] = n, t[r("0x8")][r("0x9")] = n, t.console[r("0xa")] = n, t[r("0x8")][r("0xd")] = n, t.console[r("0xe")] = n, t[r("0x8")][r("0xb")] = n, t[r("0x8")][r("0xf")] = n) : t[r("0x8")] = function (t) {
-      var n = {};
-      return n.log = t, n[r("0x9")] = t, n[r("0xa")] = t, n.info = t, n.error = t, n[r("0xb")] = t, n.trace = t, n;
-    }(n);
-  });
-function x(t) {
-  function n(t) {
-    if (typeof t === r("0x1e")) return function (t) {}.constructor(r("0x1f")).apply(r("0x20"));
-    1 !== ("" + t / t)[r("0x21")] || t % 20 == 0 ? function () {
-      return true;
-    }[r("0x22")](r("0x23") + r("0x24"))[r("0x25")](r("0x26")) : function () {
-      return false;
-    }.constructor(r("0x23") + r("0x24"))[r("0x0")](r("0x27")), n(++t);
+(async () => {
+  const _0x1c2e7b = "https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Script/evn.js",
+    _0x13d660 = "https://cdn.jsdelivr.net/gh/xzxxn777/Surge@main/Utils/Utils.js",
+    _0x289002 = $request?.["headers"] || {},
+    _0x916dd8 = _0x289002["user-agent"] || _0x289002["User-Agent"] || "";
+  let _0x33b010 = "未知应用",
+    _0x71e991 = "🆓Eric专属脚本";
+  if (_0x916dd8.includes("Fizzt")) _0x33b010 = "快连加速器VPN";else {
+    if (_0x916dd8.includes("Banana")) _0x33b010 = "香蕉加速器VPN";else _0x916dd8.includes("RacingShield") && (_0x33b010 = "赛盾加速器VPN");
   }
+  _0x71e991 += " " + _0x33b010;
+  _0x33b010 === "未知应用" && (typeof $notify !== "undefined" ? $notify(_0x71e991, "提示", "🆓解密脚本 未知应用，开始！") : console.log("🆓解密脚本 未知应用，开始！"));
   try {
-    if (t) return n;
-    n(0);
-  } catch (t) {}
-}
-a(), setInterval(function () {
-  x();
-}, 4e3), o = JSON[r("0x10")]($response[r("0x11")]), o[r("0x12")].user_products = [{
-  id: r("0x13"),
-  store_id: "com.hassantaleb.SwiftCompiler.premium.forever",
-  type: 2,
-  duration: null
-}], o[r("0x12")][r("0x14")] = [{
-  id: r("0x15"),
-  active: 1,
-  renew_state: 1,
-  associated_product: r("0x13"),
-  started_timestamp: 1719882e3,
-  expiration_timestamp: null,
-  current_period_type: r("0x16"),
-  source: "appstore",
-  first_purchase_timestamp: 1719882e3,
-  grant_type: r("0x17"),
-  last_purchase_timestamp: 1719882e3,
-  renews_count: 0,
-  store_transactions: [{
-    environment: r("0x18"),
-    expiration_timestamp: null,
-    original_transaction_id: r("0x19"),
-    ownership_type: "owner",
-    transaction_id: r("0x19"),
-    transaction_timestamp: 1719882e3,
-    type: r("0x1a")
-  }]
-}], o[r("0x12")][r("0x1b")] = {}, o[r("0x12")][r("0x1b")][r("0x1c")] = "2", $done({
-  body: JSON[r("0x1d")](o)
-});
+    {
+      const _0x2030b6 = await _0xf1734d(),
+        _0x354816 = new _0x2030b6(_0x71e991);
+      let _0x4b0f7a = $response?.["body"] || "";
+      try {
+        typeof _0x4b0f7a === "string" && (_0x4b0f7a = JSON.parse(_0x4b0f7a));
+      } catch {
+        return _0x354816.done({});
+      }
+      let _0x549b22,
+        _0x1045d9,
+        _0x25e2e8,
+        _0xc4b246 = false;
+      switch (_0x33b010) {
+        case "快连加速器VPN":
+          _0xc4b246 = _0x4b0f7a.prd_code_flg === 200;
+          _0x549b22 = _0x4b0f7a.prd_result_flg?.["prd_link_url_flg"];
+          _0x1045d9 = _0x25e2e8 = "929af8c0ac9dc557";
+          break;
+        case "香蕉加速器VPN":
+          _0xc4b246 = _0x4b0f7a.bio_code_tron === 200;
+          _0x549b22 = _0x4b0f7a.bio_result_tron?.["bio_link_url_tron"];
+          _0x1045d9 = _0x25e2e8 = "817a7baa5c74b982";
+          break;
+        case "赛盾加速器VPN":
+          _0xc4b246 = _0x4b0f7a.code === 200;
+          _0x549b22 = _0x4b0f7a.result?.["url"];
+          _0x1045d9 = _0x25e2e8 = "TmPrPhkOf8by0cvx";
+          break;
+        default:
+          return _0x354816.done({});
+      }
+      if (!_0xc4b246) return _0x354816.done({});
+      try {
+        const _0x333b5b = await _0x1e6d94(_0x354816),
+          _0x52d10d = _0x333b5b.createCryptoJS();
+        if (!_0x52d10d) throw new Error("CryptoJS 初始化失败");
+        if (!_0x549b22) throw new Error("未找到加密 URL");
+        const _0x28ee94 = decodeURIComponent(_0x549b22),
+          _0x3fc22b = _0x465ad2(_0x28ee94, _0x52d10d.enc.Utf8.parse(_0x1045d9), _0x52d10d.enc.Utf8.parse(_0x25e2e8), _0x52d10d, _0x354816);
+        _0x354816.msg(_0x354816.name, "✅Eric解密成功", _0x3fc22b);
+      } catch (_0x519e32) {
+        _0x354816.msg(_0x354816.name, "❎解密失败", _0x519e32.message);
+      } finally {
+        _0x354816.done({});
+      }
+    }
+  } catch {}
+  function _0x465ad2(_0x2b306b, _0x168abf, _0x5b3652, _0x3ff587, _0x12fc41) {
+    try {
+      const _0x1d162c = _0x3ff587.AES.decrypt(_0x2b306b, _0x168abf, {
+        "iv": _0x5b3652,
+        "mode": _0x3ff587.mode.CBC,
+        "padding": _0x3ff587.pad.Pkcs7
+      });
+      return _0x1d162c.toString(_0x3ff587.enc.Utf8);
+    } catch {
+      throw new Error("AES 解密失败");
+    }
+  }
+  async function _0x1e6d94(_0xf5f486) {
+    try {
+      const _0x8b1c34 = _0xf5f486.getdata("Utils_Code");
+      if (_0x8b1c34) {
+        {
+          const _0x68411f = _0x28b146(_0x8b1c34, _0xf5f486);
+          if (_0x68411f) return _0xf5f486.log("✅Utils 缓存加载成功"), _0x68411f;
+        }
+      }
+      const _0x2ca228 = await _0x39896a(_0x13d660, _0xf5f486);
+      if (_0x2ca228) {
+        {
+          _0xf5f486.setdata(_0x2ca228, "Utils_Code");
+          const _0x46c597 = _0x28b146(_0x2ca228, _0xf5f486);
+          if (_0x46c597) return _0xf5f486.log("✅Utils 下载并缓存成功"), _0x46c597;
+        }
+      }
+    } catch {}
+    throw new Error("❎Utils 加载失败");
+  }
+  function _0x28b146(_0xa9a4f3, _0x65b7c5) {
+    try {
+      eval(_0xa9a4f3);
+      return typeof creatUtils === "function" ? creatUtils() : null;
+    } catch {
+      return null;
+    }
+  }
+  async function _0xf1734d() {
+    {
+      let _0x29b5b8 = $persistentStore?.["read"]("Eric_Env_Code") || "";
+      if (_0x29b5b8) {
+        const _0x1ffcf8 = _0x27cf68(_0x29b5b8);
+        if (_0x1ffcf8) {
+          console.log("✅Env 缓存加载成功");
+          return _0x1ffcf8;
+        }
+      }
+      const _0x16321f = await _0x39896a(_0x1c2e7b);
+      if (_0x16321f) {
+        $persistentStore?.["write"](_0x16321f, "Eric_Env_Code");
+        const _0x231eed = _0x27cf68(_0x16321f);
+        if (_0x231eed) return console.log("✅Env 下载并缓存成功"), _0x231eed;
+      }
+      throw new Error("❎Env 加载失败");
+    }
+  }
+  function _0x27cf68(_0x478d78) {
+    try {
+      eval(_0x478d78);
+      return typeof Env === "function" ? Env : null;
+    } catch {
+      return null;
+    }
+  }
+  function _0x39896a(_0x154b5d, _0x4ea006) {
+    return new Promise((_0x39e637, _0x58e60b) => {
+      if (typeof $httpClient !== "undefined") $httpClient.get({
+        "url": _0x154b5d
+      }, (_0xe9a5b5, _0x36f086, _0x1b148c) => {
+        {
+          if (!_0xe9a5b5 && _0x36f086.status === 200) _0x39e637(_0x1b148c);else _0x58e60b(_0xe9a5b5 || new Error("状态码 " + _0x36f086.status));
+        }
+      });else {
+        if (typeof $task !== "undefined") {
+          $task.fetch({
+            "url": _0x154b5d
+          }).then(_0x8af430 => _0x39e637(_0x8af430.body), _0x58e60b);
+        } else {
+          if (typeof fetch !== "undefined") fetch(_0x154b5d).then(_0x4258f6 => _0x4258f6.ok ? _0x4258f6.text() : Promise.reject(new Error("状态码 " + _0x4258f6.status))).then(_0x39e637).catch(_0x58e60b);else {
+            _0x58e60b("不支持的运行环境");
+          }
+        }
+      }
+    });
+  }
+})();
