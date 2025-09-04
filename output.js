c8c8cdf0991df499a4222d113fffd2f3630d48a6
action <action@github.com>
2025-09-04 14:30:55 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,119 +1,85 @@
-//Tue Sep 02 2025 10:16:36 GMT+0000 (Coordinated Universal Time)
+//Thu Sep 04 2025 14:30:55 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 var encode_version = "jsjiami.com.v5";
-if ($request.url.indexOf("/strategy/listen-url/") != -1) {
-  var _0x27b8b4 = $request.headers;
-  _0x27b8b4.uid = "91414537623";
-  _0x27b8b4.Cookie = "";
-  _0x27b8b4.ce = "";
-  _0x27b8b4.bversionid = "";
-  $done({
-    headers: _0x27b8b4
-  });
-}
-if ($request.url.indexOf("/music/batchQueryMusicPolicy.do") != -1) {
-  var _0x5e6bfc = $request.headers;
-  _0x5e6bfc.uid = "91414537623";
-  _0x5e6bfc.Cookie = "";
-  _0x5e6bfc.ce = "";
-  _0x5e6bfc.bversionid = "";
-  $done({
-    headers: _0x5e6bfc
-  });
-} else {
-  if ($request.url.indexOf("user/api/my-page-header/") != -1) {
-    var _0x264071 = JSON.parse($response.body);
-    _0x264071.data.userIdentityIconItems[0].iconUrl = "https://d.musicapp.migu.cn/prod/file-service/file-down/bcb5ddaf77828caee4eddc172edaa105/7cd657454195aaeaea9e3c425491a0d0/74f77a7b9a47582a559762111ac8ba9b";
-    _0x264071.data.userIdentityIconItems[0].name = "白金会员畅听版";
-    _0x264071.data.userIdentityIconItems[0].type = "baijinhuiyuanchangtingban";
-    _0x264071.data.location = "https://t.me/GieGie777";
-    _0x264071.data.userOpNums[5].desc = "999999";
-    _0x264071.data.userOpNums[5].number = 999999;
-    var _0x50ab8a = JSON.stringify(_0x264071);
-    $done({
-      body: _0x50ab8a
-    });
-  } else {
-    if ($request.url.indexOf("/column/startup-pic-with-ad") != -1) {
-      var _0x33dad8 = JSON.parse($response.body);
-      delete _0x33dad8.data;
-      var _0x355684 = JSON.stringify(_0x33dad8);
-      $done({
-        body: _0x355684
-      });
-    } else {
-      if ($request.url.indexOf("resource/skin/list") != -1) {
-        var _0x33dad8 = JSON.parse($response.body);
-        _0x33dad8.data[0].isVip = "0";
-        var _0x355684 = JSON.stringify(_0x33dad8);
-        $done({
-          body: _0x355684
-        });
-      } else {
-        $done({});
-      }
-    }
-  }
-}
-(function (_0x44b85d, _0xfee22e, _0x55ef07) {
-  var _0x1370cb = function () {
-    {
-      var _0x3a339f = true;
-      return function (_0x6fba73, _0x7ffbfd) {
-        var _0x2b5493 = _0x3a339f ? function () {
-          {
-            if (_0x7ffbfd) {
-              var _0x56ca64 = _0x7ffbfd.apply(_0x6fba73, arguments);
-              _0x7ffbfd = null;
-              return _0x56ca64;
-            }
-          }
-        } : function () {};
-        _0x3a339f = false;
-        return _0x2b5493;
-      };
-    }
-  }();
-  var _0x1177c3 = _0x1370cb(this, function () {
-    var _0x22d5b8 = function () {};
-    var _0x383f1b = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
-    if (!_0x383f1b.console) {
-      {
-        _0x383f1b.console = function (_0x196508) {
-          var _0x55ef07 = {};
-          _0x55ef07.log = _0x196508;
-          _0x55ef07.warn = _0x196508;
-          _0x55ef07.debug = _0x196508;
-          _0x55ef07.info = _0x196508;
-          _0x55ef07.error = _0x196508;
-          _0x55ef07.exception = _0x196508;
-          _0x55ef07.trace = _0x196508;
-          return _0x55ef07;
-        }(_0x22d5b8);
-      }
-    } else {
-      {
-        _0x383f1b.console.log = _0x22d5b8;
-        _0x383f1b.console.warn = _0x22d5b8;
-        _0x383f1b.console.debug = _0x22d5b8;
-        _0x383f1b.console.info = _0x22d5b8;
-        _0x383f1b.console.error = _0x22d5b8;
-        _0x383f1b.console.exception = _0x22d5b8;
-        _0x383f1b.console.trace = _0x22d5b8;
-      }
-    }
-  });
-  _0x1177c3();
-  _0x55ef07 = "al";
+let _0x165fdc = typeof $response.body === "string" ? $response.body : JSON.stringify($response.body);
+const _0x3c6ed9 = $request.url || "";
+if (_0x3c6ed9.includes("/v1/vip/userinfo")) {
   try {
-    _0x55ef07 += "ert";
-    _0xfee22e = encode_version;
-    if (!(typeof _0xfee22e !== "undefined" && _0xfee22e === "jsjiami.com.v5")) {
-      _0x44b85d[_0x55ef07]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    const _0x10ddc2 = JSON.parse(_0x165fdc);
+    _0x10ddc2.data.is_vip = "2";
+    _0x10ddc2.data.had_vip = true;
+    _0x10ddc2.data.member_benefits[0].is_valid = "1";
+    _0x10ddc2.data.member_benefits[1].is_valid = "1";
+    _0x10ddc2.data.vip.vip_class = "2";
+    _0x10ddc2.data.vip.endtm = "99999999999999";
+    _0x10ddc2.data.vip.name = "超级会员";
+    _0x165fdc = JSON.stringify(_0x10ddc2);
+  } catch (_0x4d6303) {}
+}
+$done({
+  body: _0x165fdc
+});
+(function (_0x23b233, _0x5db6ab, _0x272338) {
+  var _0x444d63 = function () {
+    var _0x228239 = true;
+    return function (_0x180f73, _0x37df38) {
+      var _0x5e2518 = _0x228239 ? function () {
+        {
+          if (_0x37df38) {
+            var _0x4ff069 = _0x37df38.apply(_0x180f73, arguments);
+            _0x37df38 = null;
+            return _0x4ff069;
+          }
+        }
+      } : function () {};
+      _0x228239 = false;
+      return _0x5e2518;
+    };
+  }();
+  var _0x339106 = _0x444d63(this, function () {
+    {
+      var _0x11f561 = function () {};
+      var _0x4f7219 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+      if (!_0x4f7219.console) {
+        {
+          _0x4f7219.console = function (_0x4a23aa) {
+            {
+              var _0x272338 = {};
+              _0x272338.log = _0x4a23aa;
+              _0x272338.warn = _0x4a23aa;
+              _0x272338.debug = _0x4a23aa;
+              _0x272338.info = _0x4a23aa;
+              _0x272338.error = _0x4a23aa;
+              _0x272338.exception = _0x4a23aa;
+              _0x272338.trace = _0x4a23aa;
+              return _0x272338;
+            }
+          }(_0x11f561);
+        }
+      } else {
+        {
+          _0x4f7219.console.log = _0x11f561;
+          _0x4f7219.console.warn = _0x11f561;
+          _0x4f7219.console.debug = _0x11f561;
+          _0x4f7219.console.info = _0x11f561;
+          _0x4f7219.console.error = _0x11f561;
+          _0x4f7219.console.exception = _0x11f561;
+          _0x4f7219.console.trace = _0x11f561;
+        }
+      }
     }
-  } catch (_0x5b3ce8) {
-    _0x44b85d[_0x55ef07]("删除版本号，js会定期弹窗");
+  });
+  _0x339106();
+  _0x272338 = "al";
+  try {
+    _0x272338 += "ert";
+    _0x5db6ab = encode_version;
+    if (!(typeof _0x5db6ab !== "undefined" && _0x5db6ab === "jsjiami.com.v5")) {
+      _0x23b233[_0x272338]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x4e6f1b) {
+    _0x23b233[_0x272338]("删除版本号，js会定期弹窗");
   }
 })(window);
 encode_version = "jsjiami.com.v5";
